5cfffc9f942b432950f0ba4234e83345afb0f490
Ilmari Pohjola <pohjolanilmari@gmail.com>
2019-09-25 17:22:40 +0300

    readme

---
+++ Assets/Scripts/Wind.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float windThrust;
+	// Use this for initialization
+	void Start () {
+        rb = gameObject.GetComponent<Rigidbody>();
+        rb.AddForce(Random.onUnitSphere);
+	}
+	
+	// Update is called once per frame
+	private void FixedUpdate () {
+        rb.AddForce(gameObject.GetComponent<WindSensor>().GetVelocity() * windThrust);
+	}
+}
---
+++ Assets/Scripts/WindSensor.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindSensor : MonoBehaviour {
+
+    public WindZone windZone;
+    private ParticleSystem partSystem;
+    private ParticleSystem.Particle[] particles;
+    private ParticleSystem.Particle particle;
+    private ParticleSystem.EmitParams emitParams;
+
+    private Vector3 windVelocity;
+
+	void Start () {
+        windZone = gameObject.GetComponent<WindZone>();
+        partSystem = gameObject.GetComponent<ParticleSystem>();
+        particles = new ParticleSystem.Particle[partSystem.main.maxParticles];
+        this.emitParams = new ParticleSystem.EmitParams();
+        emitParams.position = transform.position;
+        emitParams.velocity = Vector3.zero;
+        emitParams.startLifetime = 0.3f;
+        partSystem.Emit(this.emitParams,1);
+        partSystem.GetParticles(particles);
+        particle = particles[0];
+        particle.remainingLifetime = Mathf.Infinity;
+        this.windVelocity = particle.velocity;
+    }
+	
+
+    private void FixedUpdate()
+    {
+        partSystem.GetParticles(particles);
+        this.particle = particles[0];
+        this.windVelocity = particle.velocity;
+
+        partSystem.Clear();
+        emitParams.position = transform.position;
+        emitParams.velocity = Vector3.zero;
+        emitParams.startLifetime = 0.3f;
+        partSystem.Emit(this.emitParams, 1);
+        partSystem.GetParticles(particles);
+        this.particle = particles[0];
+        this.particle.velocity = Vector3.zero;
+        
+    }
+
+    public Vector3 GetVelocity()
+    {
+        Debug.Log(this.windVelocity * 100);
+        return this.windVelocity;
+    }
+
+}
